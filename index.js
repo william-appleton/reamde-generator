ad5f5b8d35b1c48a2c37f76ddba559d531ddcb7f
Imprudent Friend <ImprudentFriend@gmail.com>
2020-12-23 01:00:49 -0500

    added code to write file

--- index.js
+++ index.js
@@ -57,13 +57,59 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);  
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions)
+  .then((inquirerResponses) => {
+    writeFile("README.md", generator({...inquirerResponses}));
+})
 }
 
 // function call to initialize program
 init();
+
+function generator(data) {
+    return `# ${data.title}
+
+    ## Description
+
+    ${data.description}
+
+    ## Table of Contents 
+
+    * [Installation](#installation)
+
+    * [Usage](#usage)
+
+    * [Contributing](#contributing)
+
+    * [Tests](#tests)
+
+    * [Questions](#questions)
+
+    ## Installation
+
+    Run the following command to install the required dependencies: ${data.installation}
+
+    ## Usage
+
+    ${data.usage}
+
+    ## Contributing
+
+    ${data.contributing}
+
+    ## Tests
+
+    For tests, run the following command: ${data.test}
+
+    ## Questions
+
+    I can answer any questions at ${data.email}, and more of my work can be seen at: [${data.github}](https://github.com/${data.github}/). 
+   
+    `;
+}
